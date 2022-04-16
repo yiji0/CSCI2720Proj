aed52c882a827d3196d06e2d9c1af35125782984
yiji0 <1155141508@link.cuhk.edu.hk>
2022-04-16 10:43:37 +0800

    update_jiyi

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors'); app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.json());
+
+var mongoose = require('mongoose');
+const { send } = require('express/lib/response');
+mongoose.connect('mongodb+srv://stu124:p280948-@csci2720.m2qbq.mongodb.net/stu124');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function () {
+    console.log("Connection is open...");
+
+    const LocationSchema = mongoose.Schema({
+        name: { type: String, required: true, unique: true },
+        lat: { type: mongoose.Types.Decimal128, required: true },
+        lon: { type: mongoose.Types.Decimal128, required: true },
+        comment: [{
+            uid: { type: String, required: true, unique: true },
+            content: { type: String, required: true }
+        }]
+    });
+
+    const WeatherSchema = mongoose.Schema({
+        loc: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
+        temp_c: { type: mongoose.Types.Decimal128, required: true },
+        wind_kph: { type: mongoose.Types.Decimal128, required: true },
+        wind_dir: { type: String, required: true },
+        humidity: { type: Number, required: true },
+        precip_mm: { type: mongoose.Types.Decimal128, required: true },
+        vis_km: { type: Number, required: true }
+    });
+
+    const UserSchema = mongoose.Schema({
+        id: { type: String, required: true, unique: true },
+        pwd: { type: String, required: true },
+        fav_loc: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' }
+    });
+
+    const AdminSchema = mongoose.Schema({
+        id: { type: String, required: true, unique: true },
+        pwd: { type: String, required: true }
+    });
+
+    const Location = mongoose.model('Location', LocationSchema);
+
+    const Weather = mongoose.model('Weather', WeatherSchema);
+
+    const User = mongoose.model('User', UserSchema);
+
+    const Admin = mongoose.model('Admin', AdminSchema);
+
+    app.all('/*', (req, res) => {
+        res.send("Welcome!");
+    });
+});
+
+const server = app.listen(3000);
